b53d3fbd4a00adb60cf4d69a7fc1784ee81e20d8
Deep Patel <pateldeep2332@gmail.com>
2024-08-07 20:35:21 +0530

    Adding Game Window

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,5 +1,24 @@
+#include <SFML/Graphics.hpp>
+using namespace sf;
 
 int main()
 {
+  
+    
+    RenderWindow win(VideoMode::getFullscreenModes()[0],"Fullscren Window", Style::Fullscreen);
+
+    while (win.isOpen()) {
+        Event event;
+        while (win.pollEvent(event)) {
+
+            if (event.type == Event::Closed)
+                win.close();
+        }
+
+        win.setFramerateLimit(60);
+        win.clear(Color::Blue);
+        win.display();
+    }
+
     return 0;
 }
