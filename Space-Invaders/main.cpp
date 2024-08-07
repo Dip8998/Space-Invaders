8e327230b0dec169f5003070ed9855f95615d742
Deep Patel <pateldeep2332@gmail.com>
2024-08-07 21:49:52 +0530

    Adding Random Shapes

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -16,7 +16,25 @@ int main()
         }
 
         win.setFramerateLimit(60);
-        win.clear(Color::Blue);
+        win.clear(Color::Red);
+        CircleShape circle(50);
+        circle.setFillColor(Color::Blue);
+        circle.setPosition(win.getSize().x / 2 - circle.getRadius(), win.getSize().y / 2 - circle.getRadius());
+        win.draw(circle);
+
+        RectangleShape rs(Vector2f(200,100));
+        rs.setFillColor(Color::Green);
+        rs.setPosition(Vector2f(1, -1));
+        win.draw(rs);
+
+        ConvexShape triangle;
+        triangle.setPointCount(3);
+        triangle.setPoint(0, Vector2f(400, 100));
+        triangle.setPoint(1, Vector2f(300, 500));
+        triangle.setPoint(2, Vector2f(500, 500));
+        triangle.setFillColor(Color::Black);
+        triangle.setPosition(Vector2f(700, 600));
+        win.draw(triangle);
         win.display();
     }
 
