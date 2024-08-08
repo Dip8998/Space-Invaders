77e159567cb47d2a9a1255a0258468fd175886fa
Deep Patel <pateldeep2332@gmail.com>
2024-08-08 14:02:04 +0530

    Adding Keyboard Inputs

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -33,12 +33,55 @@ public:
     void shootBullets() {
 
     }
+    void getPosition() {
+
+    }
     
 
 };
 
 int main()
 {
+    RenderWindow win(VideoMode::getFullscreenModes()[0], "Fullscreen Window", Style::Fullscreen);
+    Player player;
+
+    player.texture.loadFromFile("assets/textures/player_ship.png");
+
+    player.sprite.setTexture(player.texture);
+   
+
+    while (win.isOpen()) {
+        Event event;
+        while (win.pollEvent(event)) {
+
+            if (event.type == Event::Closed) {
+                win.close();
+            }
+
+        }
+
+        win.clear(Color::Black);
+        win.setFramerateLimit(60);
+
+
+        if (Keyboard::isKeyPressed(Keyboard::Left)) {
+            player.move();
+
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Right)) {
+
+            player.move();
+        }
+
+        player.sprite.setPosition(650, 500);
+        player.sprite.setScale(1, 1);
+        win.draw(player.sprite);
+       
+      
+
+        win.display();
+    }
+   
   
     return 0;
 }
