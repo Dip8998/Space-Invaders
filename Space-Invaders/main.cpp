f8695d42777380a5d6eb2f5ac2e7aa25df19a305
Deep Patel <pateldeep2332@gmail.com>
2024-08-08 13:14:38 +0530

    Adding player class

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -5,36 +5,40 @@ using namespace sf;
 
 class Player {
 
-
-    Texture texture;
-    Sprite sprite;
+private:
     int health = 3;
     Vector2f position = Vector2f(200, 100);
     int moveSpeed = 5;
     int playerScore = 0;
+
+
+public:
+    Texture texture;
+    Sprite sprite;
+
+    int getScore() {
+        return playerScore;
+    }
+
+    void setScore(int score) {
+        playerScore = score;
+    }
+
+    void takeDamage() {
+
+    }
+    void move() {
+
+    }
+    void shootBullets() {
+
+    }
+    
 
 };
 
 int main()
 {
   
-    Player playerOBJ;
-    cout << playerOBJ.moveSpeed;
-    /*RenderWindow win(VideoMode::getFullscreenModes()[0], "Fullscren Window", Style::Fullscreen);
-
-    while (win.isOpen()) {
-        Event event;
-        while (win.pollEvent(event)) {
-
-            if (event.type == Event::Closed)
-                win.close();
-        }
-
-        win.setFramerateLimit(60);
-        win.clear(Color::Black);
-
-        win.display();
-    }*/
-
     return 0;
 }
