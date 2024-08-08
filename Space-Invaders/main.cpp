2df2d6667c856d763f15ce42a500a09db1602d26
Deep Patel <pateldeep2332@gmail.com>
2024-08-08 10:57:26 +0530

    Adding Sprites and Logo

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -16,25 +16,47 @@ int main()
         }
 
         win.setFramerateLimit(60);
-        win.clear(Color::Red);
+        win.clear(Color::Black);
+
+        Texture texture;
+        texture.loadFromFile("assets/textures/outscal_logo.png");
+
+        Sprite outscal_sprite;
+        outscal_sprite.setTexture(texture);
+
+        outscal_sprite.setPosition(650, 100);
+        
+        outscal_sprite.setScale(1, 1);
+
+        win.draw(outscal_sprite);
+
+        Font font;
+        font.loadFromFile("assets/fonts/OpenSans.ttf");
+
+        Text text("Hello SFML!", font, 120);
+        text.setPosition(650, 300);
+        text.setFillColor(Color::Yellow);
+        win.draw(text);
+
         CircleShape circle(50);
-        circle.setFillColor(Color::Blue);
+        circle.setFillColor(Color::Red);
         circle.setPosition(win.getSize().x / 2 - circle.getRadius(), win.getSize().y / 2 - circle.getRadius());
         win.draw(circle);
 
         RectangleShape rs(Vector2f(200,100));
         rs.setFillColor(Color::Green);
-        rs.setPosition(Vector2f(1, -1));
+        rs.setPosition(Vector2f(500, 500));
         win.draw(rs);
 
         ConvexShape triangle;
         triangle.setPointCount(3);
-        triangle.setPoint(0, Vector2f(400, 100));
+        triangle.setPoint(0, Vector2f(400, 300));
         triangle.setPoint(1, Vector2f(300, 500));
         triangle.setPoint(2, Vector2f(500, 500));
-        triangle.setFillColor(Color::Black);
-        triangle.setPosition(Vector2f(700, 600));
+        triangle.setFillColor(Color::Blue);
+        triangle.setPosition(Vector2f(900, 200));
         win.draw(triangle);
+
         win.display();
     }
 
