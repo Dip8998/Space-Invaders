c7e271815251f0e7988bd8b13430a4f49a224c55
Deep Patel <pateldeep2332@gmail.com>
2024-08-12 21:17:27 +0530

    Adding Player Model and Player State enum

--- Space-Invaders/Source/GraphicService.cpp
+++ Space-Invaders/Source/GraphicService.cpp
@@ -3,41 +3,29 @@
 // Initialize Constructor 
 GraphicService::GraphicService() {
 
-	gameWindow = nullptr; // GameWindow to pointer
-	videoMode = nullptr;  // VideoMode to pointer
-
+	gameWindow = nullptr; 	videoMode = nullptr;  
 }
-// Initialize Destructor
 GraphicService::~GraphicService() {
-	onDestroy(); // Used to CleanUp Resources
-}
+	onDestroy();}
 void GraphicService::initialize() {
 
-	gameWindow = createGameWindow(); // Create new Game window to gameWindow pointer
-	gameWindow->setFramerateLimit(frameRate);
+	gameWindow = createGameWindow(); 	gameWindow->setFramerateLimit(frameRate);
 }
 RenderWindow* GraphicService::createGameWindow() {
-	setVideoMode(); // Set the videomode to Window
-
-	return new RenderWindow(*videoMode, gameWindowTitle); // Create and return new game window
-}
+	setVideoMode(); 
+	return new RenderWindow(*videoMode, gameWindowTitle); }
 void GraphicService::setVideoMode() {
-	videoMode = new VideoMode(gameWindowWidth, gameWindowHeight, VideoMode::getDesktopMode().bitsPerPixel); // Allocates and set video mode
-}
+	videoMode = new VideoMode(gameWindowWidth, gameWindowHeight, VideoMode::getDesktopMode().bitsPerPixel); }
 void GraphicService::onDestroy() {
-	delete(videoMode); // delete videomode object
-	delete(gameWindow); // delete gamewindow object
-}
+	delete(videoMode); 	delete(gameWindow); }
 void GraphicService::update(){}
 
 void GraphicService::render(){}
 
 bool GraphicService::isGameWindowOpen() {
-	return gameWindow -> isOpen(); // Return the open status for game window
+	return gameWindow -> isOpen(); 
 }
 RenderWindow* GraphicService::getGameWindow() {
-	return gameWindow; // Return game window
-}
+	return gameWindow; }
 Color GraphicService::getWindowColor() {
-	return windowColor; // Return window color
-}
+	return windowColor; }
